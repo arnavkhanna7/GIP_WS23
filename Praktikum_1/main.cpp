f910f02e512be8fe756c3fdce9a52e3c00dafe7b
arnavkhanna7 <80117887+arnavkhanna7@users.noreply.github.com>
2024-09-25 12:23:40 +0200

    Added praktikum 1

---
+++ Praktikum_1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+
+    {
+        cout << "Hallo Welt!" << endl;
+        cout << "test for git" << endl;
+    }
+
+    system("pause");
+    return 0;
+}
